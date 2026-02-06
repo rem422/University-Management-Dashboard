396a770829e4bc2864ab69fb8353d3238603e0b0
Rem Simiyu <remsimiyu@gmail.com>
2026-02-06 23:42:17 +0300

    Express JS basics

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -31,44 +31,49 @@ router.post('/', (req, res) => {
         return res.status(400).send('Please fill all fields!');
     }
 
+    const newCar = {
+        id: cars.length + 1,
+        make,
+        model,
+        year: Number(year),
+        price: Number(price)
+    };
+
+    cars.push(newCar);
     res.status(201). json({
         message: 'Product created successfully',
-        data: {
-            id,
-            make,
-            model,
-            year,
-            price
-        },
+        data: newCar
     });
 });
 
 router.put('/:id', (req, res) => {
-    const {id} = req.params;
-    const {make, model, year, price} = req.body;
-        if(!make && !model && !year && !price) {
-        return res.status(400).send('At least one field is required!');
-    }
+    const id = Number(req.params);
+    const index = cars.findIndex(c => c.id === id);
 
-    res.status(201). json({
-        message: 'Car updated successfully',
-        data: {
-            id,
-            make,
-            model,
-            year,
-            price
-    },
-});
+    if(index === -1) {
+        return res.status(404).json({error: "Car not found"});
+    };
+
+    const {make, model, year, price} = req.body;
+
+    if (make) cars[index].make = make;
+    if (model) cars[index].model = model;
+    if (year) cars[index].year = Number(year);
+    if (price) cars[index].price = Number(price);
+
+    res.json(cars[index]);
 });
 
 router.delete('/:id', (req, res) => {
-    const {id} = req.params;
+    const id = Number(req.params);
+    const index = cars.findIndex(c => c.id === id);
 
-    res.status(200).json({
-        message: 'Car deleted successfully',
-        data: id
-    })
+    if(index === -1) {
+        return res.status(404).json({err: 'Car not found'});
+    };
+
+    const deleted = cars.splice(index, 1)[0];
+    res.json({message: "Car deleted", car: deleted});
 });
 
 
