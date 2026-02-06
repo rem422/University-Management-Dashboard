6eaedb9d8f3fdc35a8fb96d9eecd7ba0656e3c45
Rem Simiyu <remsimiyu@gmail.com>
2026-02-06 15:38:21 +0300

    Node JS basics

--- 01_nodejs/server.js
+++ 01_nodejs/server.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-    console.log('Server is running on port http://localhost:3000')
+    console.log('Server is running on port http://localhost:3000');
 });
