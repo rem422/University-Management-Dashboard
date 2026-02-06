22b1afa3af895ffd2033aea619fc6ee538e61b2b
Rem Simiyu <remsimiyu@gmail.com>
2026-02-06 23:16:48 +0300

    Creating API's in express js

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -1,15 +1,78 @@
 import express from 'express';
 
 const app = express();
+const router = express.Router();
+app.use(express.json());
+
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send({message: "Hello from the Cars API!"})
+let cars = [
+    {id: 1, make:'Toyota', model: 'Camry', year: 2022, price: 2800},
+    {id: 2, make:'Tesla', model: 'Model S', year: 2023, price: 2500},
+    {id: 3, make:'Ford', model: 'F-150', year: 2021, price: 3500},
+];
+
+router.get('/', (req, res) => {
+    res.json(cars);
 });
 
-app.get('/api/v1/cars', (req, res) => {
-    res.send('All cars')
+router.get('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const car = cars.find((car) => car.id === id);
+
+    if(!car) return res.status(404).send('Car not found');
+    res.json(car);
 });
+
+router.post('/', (req, res) => {
+    const {id, make, model, year, price} = req.body;
+
+    if(!id, !make, !model, !year, !price) {
+        return res.status(400).send('Please fill all fields!');
+    }
+
+    res.status(201). json({
+        message: 'Product created successfully',
+        data: {
+            id,
+            make,
+            model,
+            year,
+            price
+        },
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const {make, model, year, price} = req.body;
+        if(!make && !model && !year && !price) {
+        return res.status(400).send('At least one field is required!');
+    }
+
+    res.status(201). json({
+        message: 'Car updated successfully',
+        data: {
+            id,
+            make,
+            model,
+            year,
+            price
+    },
+});
+});
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+
+    res.status(200).json({
+        message: 'Car deleted successfully',
+        data: id
+    })
+});
+
+
+app.use('/api/v1/cars', router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT || 3000}`);
